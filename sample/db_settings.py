477aa002bb0624d609a5e853218199bd8f85d8fb
Fabiano Oliveira <email@fabiano.pro>
2016-02-27 01:21:51 -0300

    Database config sample

---
+++ sample/db_settings.py
@@ -0,0 +1,8 @@
+###
+## It is the database configuration and credentials
+## Replace the values removing the curly brackets {} inside the quotes
+###
+uid=u'{Database User}'
+pwd=u'{Database password}'
+host="{Database Host}"
+database="{Database name}"
